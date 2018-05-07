c447380acdd148947ee188573f22f594b33cf70e
zhenyuli1216 <31937095+zhenyuli1216@users.noreply.github.com>
2018-05-06 20:04:41 -0400

    data prep

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -1,1 +1,27 @@
 #data processing
+import numpy as np 
+import os 
+import random 
+import pickle 
+import csv 
+import pandas as pd 
+
+y = []
+X = []
+
+df = pd.read_csv('../Data/bitcoinData.csv')
+df = df.dropna()
+data = df.as_matrix()[:,1:]
+
+#split into samples 
+samples = list()
+length = 223
+for i in range(0, len(data), length):
+	sample = data[i:i+length]
+	samples.append(sample)
+
+#13 samples and 223 time steps per sample 
+
+y = data[:,0].reshape((len(samples), length, 1))
+X = data[:,1:].reshape((len(samples), length, data[:,1:].shape[1]))
+print (X)
